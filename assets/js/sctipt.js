0d450f60085248cccaea9d0988a9a290bf3c85ee
Rohit <rohitsraj12@gmail.com>
2021-10-05 16:51:37 +0530

    Update sctipt.js

--- assets/js/sctipt.js
+++ assets/js/sctipt.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
-    
+    $(".hamburger").click(function(){
 
+        // if(){
+
+        // }
+        $(".header__nav").animate({
+            width: "toggle"
+            // left: "-100%"
+        });
+    })
 
 })
